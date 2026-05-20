421a81c9340855273913bcaab45a335434d9b27d
Abdulquadri Olatunbosun <olaolat@hotmail.com>
2026-05-20 15:06:19 +0100

    Modify workflow and app

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'Hello, World!',
+        'message': 'Hello, World',
         'deployed_on': 'kubernetes'
     })
 
